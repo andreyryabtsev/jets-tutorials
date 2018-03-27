ed6edd2cc488e9075a171186edc52cadeffe0870
Andrey Ryabtsev <ryabtsev@uw.edu>
2018-03-27 13:13:59 -0700

    added first exercise

---
+++ rng2.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+arr = np.empty(1000)
+for i in range(1, 1000):
+    arr[i-1] = 5 + 10 * random.random()
+plt.hist(arr)
+plt.title("Random uniform numbers")
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+plt.show()
